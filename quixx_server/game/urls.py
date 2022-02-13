cfed374e6735b0b9eb78df08fb0a1e4051e940a7
ixns <dvzvdv@outlook.com>
2022-02-12 21:03:30 -0500

    added QuixxGameView, QuixxGamesIndexView, and added them to urls

--- quixx_server/game/urls.py
+++ quixx_server/game/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "game"
-
 urlpatterns = [
-    path('', views.game_index, name='game_index')    
+	path('', views.QuixxGamesIndexView.as_view(), name='quixx_games_index'),
+	path('<int:pk>/', views.QuixxGameView.as_view(), name='quixx_single_game_view'),
 ]
--- quixx_server/game/views.py
+++ quixx_server/game/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views.generic import *
 
-def game_index(request):
-	template = loader.get_template('polls/index.html')
-	return HttpResponseRedirect(template.render(request))
+from .models import QuixxGame
+
+class QuixxGamesIndexView(ListView):
+	template_name = 'game/index.html'
+	context_object_name = 'latest_games_list'
+
+	def get_queryset(self):
+		"""
+		Return all open Quixx games
+		"""
+		return QuixxGame.objects
+	
+
+class QuixxGameView(DetailView):
+	model = QuixxGame
+	template_name = 'game/game.html'
+	
+	def get_queryset(self):
+		"""
+		Excludes games that aren't published yet.
+		"""
+		return QuixxGame.objects.filter(pub_date__lte=timezone.now())
+	
